f39c4fdb15c33fdc562f81b4f68ec3f6db84b77d
saggidinithin <saggidinithin@gmail.com>
2026-02-10 21:39:15 +0530

    Add files via upload

---
+++ if.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if(num > 0)
+    {
+        printf("The number is Positive");
+    }
+
+    return 0;
+}
